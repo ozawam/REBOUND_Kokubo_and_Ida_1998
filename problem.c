6b5a98b9c54039d1fc1025fbc1c676ee6f935e0c
ozawam <ozawa_m@eps.s.u-tokyo.ac.jp>
2019-03-15 14:05:18 +0900

    N=1000 leap-frog

--- problem.c
+++ problem.c
@@ -11,12 +11,18 @@ int main(int argc, char* argv[]){
     struct reb_simulation* r = reb_create_simulation();
     r->dt                   = 0.001*2.*M_PI;                // initial timestep
  //   r->integrator           = REB_INTEGRATOR_IAS15;
-    r->integrator    = REB_INTEGRATOR_MERCURIUS;
+ //   r->integrator    = REB_INTEGRATOR_MERCURIUS;
+    r->integrator           = REB_INTEGRATOR_LEAPFROG;
     r->collision            = REB_COLLISION_DIRECT;
+ //   r->collision    = REB_COLLISION_TREE;
     r->collision_resolve    = reb_collision_resolve_merge;        // Choose merger collision routine.
     r->heartbeat            = heartbeat;
     r->hash_ctr             = 0;
 
+ //   double boxsize = 3.0;
+ //   reb_configure_box(r,boxsize,1,1,1);
+
+   // Star
     struct reb_particle star = {0};
     star.m = 1;
     star.r = 0.1;
@@ -39,9 +45,9 @@ int main(int argc, char* argv[]){
 */
    // Planetesimal disk parameters
     double M_sun=1.989*pow(10.0,33.0);//g
-    double total_disk_mass = 4000.0* 3.0*pow(10.0,26.0)/M_sun;
-    int N_planetesimals = 7;
-    double planetesimal_mass = 1e-4;  // total_disk_mass/N_planetesimals;
+    double total_disk_mass = 4000.0* 3.0*pow(10.0,23.0)/M_sun;
+    int N_planetesimals = 1000;
+    double planetesimal_mass = total_disk_mass/N_planetesimals;
     double delta_a = 0.085;
     double central_a = 1.0;
     double amin = central_a - (delta_a/2.0), amax = central_a + (delta_a/2.0);   //planet at inner edge of disk
@@ -60,11 +66,13 @@ int main(int argc, char* argv[]){
         double phi     = reb_random_uniform(0,2.*M_PI);
 
         double rho = 2.0;
-        double one_au=1.49597870*pow(10.0,13.0);//cm
+        double one_au = 1.49597870*pow(10.0,13.0);//cm
+  
+        double radius_f = 6.0;//fold enlargement(radius enhancement)
 
         pt = reb_tools_orbit_to_particle(r->G, star, planetesimal_mass, a, e, inc, Omega, apsis, phi);
         pt.m = planetesimal_mass;
-        pt.r =   4e-2;// pow((3.0*planetesimal_mass)/(4.0*M_PI*rho) ,1.0/3.0)/ one_au; //unit is AU.
+        pt.r = pow((3.0*planetesimal_mass*M_sun)/(4.0*M_PI*rho) ,1.0/3.0)/ one_au * radius_f; //unit is AU.
         pt.lastcollision = 0;
         reb_add(r, pt);
     }
@@ -80,8 +88,11 @@ void heartbeat(struct reb_simulation* r){
      //   int snap_n = (int)(r->t)/(100.0*2.0*M_PI);
         int snap_n = (r->hash_ctr);
         char SNAP[30];
-    if (reb_output_check(r, 100.0*2.0*M_PI)){  
+    if (reb_output_check(r, 1.0*2.0*M_PI)){  
         reb_output_timing(r, 30000);
+           }
+
+    if (reb_output_check(r, 10.0*2.0*M_PI)){
        sprintf(SNAP,"output/snap01/snap%05d.dat",snap_n);
        reb_output_orbits(r,SNAP);
    //    reb_output_orbits(r,"output/snap01/snap.dat");
